6ae620cdd159f1270a9615fd8d0c01934dec6cf7
Jakub Zieliński <zielu965@gmail.com>
2025-11-15 13:15:14 +0100

    lab2zad5
    lab2zad5

---
+++ lab2zad5/lab2zad5/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using lab2zad5;
+
+Sumator sumator = new Sumator(new int[] { 11, 2, 58, 62, 14, 74, 3, 5, 7, 102, 26, 90 });
+
+Console.WriteLine("Liczba elementów w tablicy: ");
+sumator.IleElementow();
+
+Console.WriteLine("\nWypisanie elementów tablicy: ");
+sumator.WypiszElementy();
+
+Console.WriteLine("\nWypisanie elementów tablicy w zakresie od 3 do 7: ");
+sumator.WypiszElementyZakres(3, 7);
+
+Console.WriteLine("\nSuma liczb podzielnych przez 2 : " + sumator.SumaPodziel2());
+
+Console.WriteLine("\nSuma wszystkich liczb: " + sumator.Suma());
---
+++ lab2zad5/lab2zad5/Sumator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2zad5
+{
+    internal class Sumator
+    {
+        private int[] Liczby { get; set; }
+        public Sumator(int[] liczby)
+        {
+            Liczby = liczby;
+        }
+        public int Suma()
+        {
+            int sumaLiczb = 0;
+
+            for (int i =0; i < Liczby.Length; i++)
+            {
+                sumaLiczb += Liczby[i];
+            }
+            return sumaLiczb;
+        }
+        public int SumaPodziel2()
+        {
+            int sumaLiczb = 0;
+            for (int i = 0; i < Liczby.Length; i++)
+            {
+                if (Liczby[i] % 2 == 0)
+                {
+                    sumaLiczb += Liczby[i];
+                }
+            }
+            return sumaLiczb;
+        }
+        public int IleElementow()
+        { return Liczby.Length;
+        }
+        public void WypiszElementy()
+        {
+            for (int i = 0; i < Liczby.Length; i++)
+            {
+                Console.Write(Liczby[i] + " ");
+            }
+        }
+        public void WypiszElementyZakres(int lowIndex, int highIndex)
+        { for (int i = lowIndex; i <= highIndex; i++) 
+            { if (i >=0 && i < Liczby.Length)
+                { Console.WriteLine(Liczby[i]);
+                }
+} }
+    }
+}
