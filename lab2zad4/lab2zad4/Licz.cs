0d33e3c5cb0159fe8760d943ffb5883075760f79
Jakub Zieliński <zielu965@gmail.com>
2025-11-15 12:24:28 +0100

    lab2zad4
    lab2zad4

---
+++ lab2zad4/lab2zad4/Licz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2zad4
+{
+    internal class Licz
+    {
+        private int value;
+        public Licz(int initialValue)
+        {
+            value = initialValue;
+        }
+        public void Dodaj(int toAdd)
+        {             value += toAdd;
+        }
+        public void Odejmij(int toSubtract)
+        {
+            value -= toSubtract;
+        }
+        public void WypiszStan()
+        {
+            Console.WriteLine("Aktualna wartość: " + value);
+        }
+    }
+}
