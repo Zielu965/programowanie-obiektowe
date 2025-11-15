30bf24149773eb596ee5238252425ccd7c748f5f
Jakub Zieliński <zielu965@gmail.com>
2025-11-15 10:59:51 +0100

    lab2zad1
    lab2zad1

---
+++ lab2zad2/lab2zad2/Osoba.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2zad2
+{
+    internal class Osoba
+    {
+
+        private string imie, nazwisko;
+        private int wiek;
+
+        public string Imie
+        {
+            get
+            {
+                return imie;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    imie = value;
+
+                }
+                else
+                {
+                    throw new ArgumentException("Imie musi zawierać co najmniej 2 znaki.");
+
+                }
+            }
+        }
+        public string Nazwisko
+        {
+            get
+            {
+                return nazwisko;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    nazwisko = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Nazwisko musi zawierać co najmniej 2 znaki.");
+                }
+            }
+        }
+        public int Wiek
+        {
+            get
+            {
+                return wiek;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    wiek = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Wiek musi być liczbą dodatnia.");
+                }
+            }
+        }
+
+        public Osoba(string imie, string nazwisko, int wiek)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+        }
+        public void WyswietlInformacje()
+        {
+            Console.WriteLine($"Imię: {Imie}, Nazwisko: {Nazwisko}, Wiek: {Wiek}");
+        }   
+    }
+}
