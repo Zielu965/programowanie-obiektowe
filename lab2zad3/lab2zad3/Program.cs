90dcafd27cade0897f74c3f58b7ed1c69c91df2b
Jakub Zieliński <zielu965@gmail.com>
2025-11-15 11:40:51 +0100

    lab2zad3
    zadanie3lab2

---
+++ lab2zad3/lab2zad3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using lab2zad3;
+
+Student student = new Student("Jan", "Koalski");
+student.DodajOcene(4);
+student.DodajOcene(3);
+student.DodajOcene(5);
+student.DodajOcene(2);
+student.DodajOcene(1);
+Console.WriteLine($"Średnia ocen studenta {student.Imie} {student.Nazwisko} wynosi: {student.SredniaOcen}");
---
+++ lab2zad3/lab2zad3/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2zad3
+{
+    internal class Student
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        private List<int> oceny;
+
+        public Student(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            oceny = new List<int>();
+        }
+        public double SredniaOcen
+        {
+            get
+            {
+                if (oceny.Count == 0)
+                    return 0;
+                return oceny.Average();
+            }
+
+        }
+        public void DodajOcene(int ocena)
+        {
+            oceny.Add(ocena);
+        }
+    }
+}
